8d2a58bdf1c1760ed56201a60d6d67c9afa811a4
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-08-08 11:45:31 -0400

    Add event listeners for form buttons

--- src/modules/render.js
+++ src/modules/render.js
@@ -5,11 +5,12 @@ import Task from "./task";
 const toDo = new ToDoList();
 
 function addEventHandlers() {
-    const newListButton = document.querySelector(".new-list-button")
-    const newTaskButton = document.querySelector(".new-task-button")
-
     const listDialog = document.querySelector(".list-dialog")
     const taskDialog = document.querySelector(".task-dialog")
+
+    // For new task and new list buttons
+    const newListButton = document.querySelector(".new-list-button")
+    const newTaskButton = document.querySelector(".new-task-button")
 
     newListButton.addEventListener("click", () => {
         listDialog.style.display = "flex"
@@ -19,6 +20,32 @@ function addEventHandlers() {
     newTaskButton.addEventListener("click", () => {
         taskDialog.style.display = "flex"
         listDialog.style.display = "none"
+    })
+
+    // for list form buttons
+    const listSubmit = document.getElementById("list-submit")
+    const listClose = document.querySelector(".list-close")
+
+    listSubmit.addEventListener("click", (e) => {
+        e.preventDefault()
+        listDialog.style.display = "none"
+    })
+
+    listClose.addEventListener("click", () => {
+        listDialog.style.display = "none"
+    })
+
+    // for task form buttons
+    const taskSubmit = document.getElementById("task-submit")
+    const taskClose = document.querySelector(".task-close")
+
+    taskSubmit.addEventListener("click", (e) => {
+        e.preventDefault()
+        taskDialog.style.display = "none"
+    })
+
+    taskClose.addEventListener("click", () => {
+        taskDialog.style.display = "none"
     })
 }
 

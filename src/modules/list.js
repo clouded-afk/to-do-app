af7b16f0b80cb8cd3008e40abe6a761c63a3f2b8
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-08-09 13:41:10 -0400

    Add functions to class

--- src/modules/list.js
+++ src/modules/list.js
@@ -3,4 +3,31 @@ export default class List {
         this.name = name
         this.tasks = []
     } 
+
+    setName(name) {
+        this.name = name
+    }
+
+    getName() {
+        return this.name
+    }
+
+    setTasks(tasks) {
+        this.tasks = tasks
+    }
+
+    getTasks() {
+        return this.tasks
+    }
+
+    getTask(taskName) {
+        return this.tasks.find((task) => task.getName() === taskName)
+    }
+
+    addTask(newTask) {
+        if (this.tasks.find((task) => task.getName() === newTask)) {
+            return
+        }
+        this.tasks.push(newTask)
+    }
 }

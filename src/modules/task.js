17d0740188b3c06525112f9f9e5a01e4cfb8d368
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-08-12 16:51:57 -0400

    Add getters/setters to classes

--- src/modules/task.js
+++ src/modules/task.js
@@ -5,4 +5,37 @@ export default class Task {
         this.dueDate = dueDate
         this.priority = priority
     }
+
+    setName(name) {
+        this.name
+    }
+
+    getName() {
+        return this.name
+    }
+
+    setDescription(description) {
+        this.description
+    }
+
+    getDescription() {
+        return this.description
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate
+    }
+
+    getDueDate() {
+        return this.dueDate
+    }
+
+    setPriority(priority) {
+        this.priority
+    }
+
+    getPriority() {
+        return this.priority
+    }
+    
 }
--- src/modules/todolist.js
+++ src/modules/todolist.js
@@ -8,4 +8,23 @@ export default class ToDoList {
         this.lists.push(new List("Today"))
         this.lists.push(new List("This Week"))
     }
+
+    setLists(lists) {
+        this.lists = lists
+    }
+
+    getLists() {
+        return this.lists
+    }
+
+    getList(listName) {
+        return this.lists.find((list) => list.getName() === listName)
+    }
+
+    addList(newList) {
+        if (this.lists.find((list) => list.name === newList.name)) {
+            return
+        }
+        this.lists.push(newList)
+    }
 }

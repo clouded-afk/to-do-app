ca8fd70ffe03b34ea038ddf9ec0d2af82c7cfe5b
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-09-03 14:32:56 -0400

    Add ability to save and grab toDo

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,3 +1,19 @@
 import Task from "./task"
 import List from "./list"
 import ToDoList from "./todolist"
+
+export default class Storage {
+    saveToDo(data) {
+        localStorage.setItem("toDo", JSON.stringify(data))
+    }
+
+    getToDoList() {
+        const toDO = Object.assign(new ToDoList(), JSON.parse(localStorage.getItem("toDo")))
+
+        toDO.setLists(toDO.getLists().map((list) => Object.assign(new List(), list)))
+
+        toDO.getLists().forEach((list) => list.setTasks(list.getTasks().map((task) => Object.assign(new Task(), task))))
+
+        return toDO
+    }
+}
